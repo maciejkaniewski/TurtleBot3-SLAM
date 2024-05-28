d49d66b1f17443b0f05c5d447a3badf0ccc23941
Maciej Kaniewski <maciejkaniewski1999@gmail.com>
2024-05-28 11:57:08 +0200

    Create histogram_filter package

---
+++ ros2_ws/src/histogram_filter/histogram_filter/histogram_filter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+
+
+class HistogramFilter(Node):
+    def __init__(self):
+        super().__init__("histogram_filter")
+        self.get_logger().info("HistogramFilter node started.")
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = HistogramFilter()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
