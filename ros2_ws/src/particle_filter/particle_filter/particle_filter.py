bc266781cc0a13e4d6df368dcc5d4aa726e0a6bb
Maciej Kaniewski <maciejkaniewski1999@gmail.com>
2024-06-06 18:28:16 +0200

    Implement creation of particles with uniform distribution

--- ros2_ws/src/particle_filter/particle_filter/particle_filter.py
+++ ros2_ws/src/particle_filter/particle_filter/particle_filter.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+import random
+import numpy as np
 import rclpy
 from rclpy.node import Node
+from particle import Particle
 
 
 class ParticleFilter(Node):
@@ -17,6 +20,48 @@ class ParticleFilter(Node):
         super().__init__("particle_filter")
         self.get_logger().info("particle_filter node started.")
 
+        # Declare parameters with default values
+        self.declare_parameter("num_particles", 1000)
+        self.declare_parameter("x_range_m", [-2.25, 2.25])
+        self.declare_parameter("y_range_m", [-2.25, 2.25])
+        self.declare_parameter("theta_range_deg", [0, 360])
+
+        # Get parameters
+        self.num_particles = self.get_parameter("num_particles").get_parameter_value().integer_value
+        self.x_range_m = tuple(self.get_parameter("x_range_m").get_parameter_value().double_array_value)
+        self.y_range_m = tuple(self.get_parameter("y_range_m").get_parameter_value().double_array_value)
+        self.theta_range_deg = tuple(self.get_parameter("theta_range_deg").get_parameter_value().integer_array_value)
+
+        # Log the parameters
+        self.get_logger().info(f"num_particles: {self.num_particles}")
+        self.get_logger().info(f"x_range_m: {self.x_range_m}")
+        self.get_logger().info(f"y_range_m: {self.y_range_m}")
+        self.get_logger().info(f"theta_range_deg: {self.theta_range_deg}")
+
+        # Create particles
+        self.particles = self.create_uniform_particles(self.num_particles, self.x_range_m, self.y_range_m, self.theta_range_deg)
+
+    def create_uniform_particles(self, num_particles: int, x_range: tuple, y_range: tuple, theta_range: tuple) -> list:
+        """
+        Create uniform distribution particles.
+
+        Args:
+            num_particles (int): The number of particles.
+            x_range_m (tuple): The x-coordinate range.
+            y_range_m (tuple): The y-coordinate range.
+            theta_range_deg (tuple): The theta range.
+
+        Returns:
+            list: The list of particles.
+        """
+
+        particles = []
+        for _ in range(num_particles):
+            x = random.uniform(x_range[0], x_range[1])
+            y = random.uniform(y_range[0], y_range[1])
+            theta = random.uniform(np.deg2rad(theta_range[0]), np.deg2rad(theta_range[1])) % (2 * np.pi)
+            particles.append(Particle(x, y, theta, 1.0 / num_particles))
+        return particles
 
 def main(args=None):
     rclpy.init(args=args)
